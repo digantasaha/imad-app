0ea8004e8fb23c03bbe6801c08a3a057e9539cd8
digantasaha <36476556+digantasaha@users.noreply.github.com>
2018-02-14 23:24:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ function createTemplate(data){
     var heading=data.heading;
     var content=data.content;
 }
-var htmlTemplate=`
+var htmlTemplate= `
 <html>
     <head>
         <title>${title}</title>
