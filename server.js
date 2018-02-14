1d04dd86dfefd680b91a5a26da199bf37e249ba6
digantasaha <36476556+digantasaha@users.noreply.github.com>
2018-02-14 21:51:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.send('Articel one was requested and will be served here');
+   res.sendFile(path.join(_dirname, 'ui', 'article-one.html'));
 })
 
 app.get('/article-two',function(req,res){
