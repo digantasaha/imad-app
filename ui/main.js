d32a3e2d74c37c73872afe7e66a94eb30ab43a98
digantasaha <36476556+digantasaha@users.noreply.github.com>
2018-02-16 18:47:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+
+//change the value of main-text div
+
+var element=document.getElementById('main-text');
+
+element.innerHTML='SOmething New!!!';
+// move the image
+var element1=document.getElementById('image');
+element1.onclick= function() {
+  image.style.marginLeft= '200px';  
+};
